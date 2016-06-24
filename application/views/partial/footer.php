ceceba5069c243705c220191188c5588664e6519
gfxgee <boygee2025@gmail.com>
2016-06-24 17:10:10 +0800

    modified:   application/views/partial/footer.php

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -2,12 +2,10 @@
 	</div>
 
 	<div id="footer">
-		<div class="jumbotron push-spaces hide">
-			<?php echo $this->lang->line('common_you_are_using_ospos'); ?>
-			<?php echo $this->config->item('application_version'); ?>.
-			<?php echo $this->lang->line('common_please_visit_my'); ?>
-			<a href="https://github.com/jekkos/opensourcepos" target="_blank"><?php echo $this->lang->line('common_website'); ?></a>
-			<?php echo $this->lang->line('common_learn_about_project'); ?>
+		<div class="jumbotron push-spaces">
+			<a class="" href="<?php echo site_url(); ?>"><img src="<?php echo base_url();?>/images/possim_logo.png" width="150"></a>
+			<p style="font-size: 12px;">Point of Sale and Store Inventory System</p>
+			&copy; POSSIM <?php echo date('Y'); ?>
 		</div>
 	</div>
 </body>
